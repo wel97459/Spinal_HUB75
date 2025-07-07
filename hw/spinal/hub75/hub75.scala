a414dc3198c65350c36cf0804ad51215c0615d84
wel97459 <wel97459@gmail.com>
2025-07-06 22:38:44 -0700

    bouching ball

--- hw/spinal/hub75/hub75.scala
+++ hw/spinal/hub75/hub75.scala
@@ -153,9 +153,13 @@ case class hub75_top() extends Component {
 
 /***-Wires-***/
 
-    val redValue = Bits(8 bits)
-    val greenValue = Bits(8 bits)
-    val blueValue = Bits(8 bits)
+    val redValue0 = Bits(8 bits)
+    val greenValue0 = Bits(8 bits)
+    val blueValue0 = Bits(8 bits)
+
+    val redValue1 = Bits(8 bits)
+    val greenValue1 = Bits(8 bits)
+    val blueValue1 = Bits(8 bits)
 
 /***-Streams-***/
 
@@ -163,39 +167,77 @@ case class hub75_top() extends Component {
     val glow = new PWM_Test(0)
     val hub = new hub75_Test()
 
+    val ball = new BasicBall(64, 63) 
+    
     glow.io.start := False
     glow.io.Wait := False
 
     hub.io.start := False
     hub.io.Wait := False
 
+    ball.io.reset := False
 /***-Routing-***/
+    val ballU = False 
+    val ballL = False
+    val X = 64 - hub.io.X
+
+    when(X === ball.io.X && a(4 downto 0) === ball.io.Y && ball.io.Y < 32){
+        ballU := True
+    }
+
+    when(X === ball.io.X && a(4 downto 0) === (ball.io.Y - 32) && ball.io.Y > 31){
+        ballL := True
+    }
+
     io.hub75.Sclk := hub.io.Sclk
     io.hub75.Latch := hub.io.Latch
-    io.hub75.RGB0.R := (glow.io.mask & redValue).asBits =/= 0
-    io.hub75.RGB0.G := (glow.io.mask & greenValue).asBits =/= 0
-    io.hub75.RGB0.B := (glow.io.mask & blueValue).asBits =/= 0
-    io.hub75.RGB1.R := False
-    io.hub75.RGB1.G := False
-    io.hub75.RGB1.B := False
+    
+    io.hub75.RGB0.R := (glow.io.mask & redValue0).asBits =/= 0
+    io.hub75.RGB0.G := (glow.io.mask & greenValue0).asBits =/= 0
+    io.hub75.RGB0.B := (glow.io.mask & blueValue0).asBits =/= 0
+
+    io.hub75.RGB1.R := (glow.io.mask & redValue1).asBits =/= 0
+    io.hub75.RGB1.G := (glow.io.mask & greenValue1).asBits =/= 0
+    io.hub75.RGB1.B := (glow.io.mask & blueValue1).asBits =/= 0
 
     io.hub75.Blank := !glow.io.blank
 
     io.hub75.Address := a(4 downto 0).asBits
     
 /***-LutChains-***/
-    redValue := (64 - hub.io.X).asBits;
-    greenValue := B"000" ## a(4 downto 0).asBits
-    blueValue := 2;
+    when(ballU){
+        redValue0 := 255
+        greenValue0 := 255
+        blueValue0 := 255
+    }otherwise{
+        redValue0 := 2
+        greenValue0 := 2
+        blueValue0 := 2
+    }
+
+    when(ballL){
+        redValue1 := 255
+        greenValue1 := 255
+        blueValue1 := 255
+    }otherwise{
+        redValue1 := 2
+        greenValue1 := 2
+        blueValue1 := 2
+    }
 
 /***-Logic-***/
     when(glow.io.done.rise()){
         a := a + 1
     }
 
+    ball.io.update := (a === 64).rise()
+
     val InterfaceFMS = new StateMachine {
         /***-FMS-***/
         val Reset: State = new StateDelay(10) with EntryPoint {
+            whenIsActive{
+                ball.io.reset := True
+            }
             whenCompleted {
                 goto(SendData)
             }
@@ -226,6 +268,50 @@ case class hub75_top() extends Component {
         }
     }
 
+}
+
+case class BasicBall(val SX: BigInt, val SY: BigInt) extends Component
+{
+    val io = new Bundle {
+        val update = in Bool()
+        val reset = in Bool()
+        val X = out UInt(8 bits)
+        val Y = out UInt(8 bits)
+    }
+
+    val ballX = Reg(UInt(8 bits)) init(0)
+    val ballY = Reg(UInt(8 bits)) init(10)
+    val ballXDir = Reg(Bool) init(True)
+    val ballYDir = Reg(Bool) init(True)
+
+    io.X := ballX
+    io.Y := ballY
+    when(io.reset){
+        ballX := 0
+        ballY := 10
+        ballXDir := True
+        ballYDir := True
+    }elsewhen(io.update.fall()){
+        when(ballX < 1){
+            ballXDir := True
+        } elsewhen(ballX >= SX){
+            ballXDir := False
+        }
+
+        when(ballY < 1) {
+            ballYDir := True
+        } elsewhen(ballY >= SY){
+            ballYDir := False
+        }
+    }elsewhen(io.update.rise()){
+        when(ballXDir) {
+            ballX := ballX + 1
+        } otherwise (ballX := ballX - 1)
+
+        when(ballYDir) {
+            ballY := ballY + 1
+        } otherwise (ballY := ballY - 1)
+    }
 }
 
 object Hub75Sim extends App {
